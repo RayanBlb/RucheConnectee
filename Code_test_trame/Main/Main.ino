0a3696a7d5087de237e4aa1bff96535b06093d7b
RayanBlb <rayan.benlacheheb.cours@gmail.com>
2021-11-14 22:51:46 +0100

    Rayan - add - Ajout information sur la trame

--- Code_test_trame/Main/Main.ino
+++ Code_test_trame/Main/Main.ino
@@ -19,7 +19,16 @@ void setup() {
 void debug_trame(){
   
   //Trame forme : I|macAdresse|son|temperature|CO2|TVOC|Piezo|code_erreur
-  
+
+  /* I = payload[0] | 1 * uint8_t
+  macAdresse = payload[1] to payload[6] | 6 * uint8_t
+  son = payload[7] et payload[8] | 1 * uint8_t
+  temperature = payload[9] | 1 * uint8_t
+  CO2 = payload[10] et payload[11] | 2 * uint8_t
+  CO2_TVOC = payload[12] et payload[13] | 2 * uint8_t
+  Piezo = payload[14] et payload[15] | 2 * uint8_t
+  code_erreur = 1 * uint8_t <-- information transmises via la fonction "LoRa_send_payload" */
+
   build_trame(payload);
 
   uint16_t son = (payload[7] * 256u) + payload[8];
