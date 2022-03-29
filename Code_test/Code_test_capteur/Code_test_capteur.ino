b4364e3ebb107b8b3315ddf116445ded8986ff85
Olivier-ca <osa.camarasa@gmail.com>
2022-03-29 14:26:26 +0200

    Olivier - add - code de test capteurs

--- Code_test/Code_test_capteur/Code_test_capteur.ino
+++ Code_test/Code_test_capteur/Code_test_capteur.ino
@@ -1,42 +1,46 @@
 #include "Adafruit_CCS811.h" //librerie du capteur CO2
 #include "arduinoFFT.h"
-#include "WiFi.h"
-#include "LoRa.h"
 
-Adafruit_CCS811 ccs; //permet d'utiliser le capteur CO2
-
-float cap_temp;
-uint16_t cap_co2;
-uint16_t cap_tvoc;
-
+//Structure données
 struct data_trame { 
-  char type; 
+  char type;
   uint8_t mac[6];
   double son;
-  float temperature;
+  double Piezo;
   uint16_t CO2;
   uint16_t CO2_TVOC;
-  double Piezo;
-  uint16_t hygro;
+  float temperature;
+  uint8_t hygro;
   uint16_t v_bat;
-  uint16_t charg_bat;
-  uint16_t IA;
+  uint8_t charg_bat;
+  uint8_t IA;
   int erreur;
 } ds;
 
-/**************************************/
-//  Variable Lora                     //
-/**************************************/
+//Variable son
+const uint16_t samples = 1024; //This value MUST ALWAYS be a power of 2 
+const double samplingFrequency = 2000; //Hz, must be less than 10000 due to ADC 
 
-//define the pins used by the LoRa transceiver module
-#define SCK 18
-#define MISO 19
-#define MOSI 23
-#define CS 18
+unsigned int sampling_period_us;
+unsigned long microseconds;
+       
+double vReal[samples];
+double vImag[samples];
 
-#define SS 5
-#define RST 14
-#define DI0 26
+//Variable piezo
+const uint16_t samples_piezo = 1024; //This value MUST ALWAYS be a power of 2 
+const double samplingFrequency_piezo = 2000; //Hz, must be less than 10000 due to ADC 
+
+unsigned int sampling_period_us_piezo;
+unsigned long microseconds_piezo;
+       
+double vReal_piezo[samples_piezo];
+double vImag_piezo[samples_piezo];
+
+Adafruit_CCS811 ccs; //permet d'utiliser le capteur CO2 et temperature
+
+arduinoFFT FFTP = arduinoFFT();
+arduinoFFT FFT = arduinoFFT();
 
 /**************************************/
 //  Variable Fonction                 //
@@ -52,14 +56,14 @@ struct data_trame {
 #define CHANNEL_PIEZO 2
 
 //Taille payload
-#define PAYLOAD_LEN 25
-
+#define PAYLOAD_LEN 20
 
 void setup() {
+  Serial.begin(9600);
   pinMode(MEASURE, OUTPUT);
   digitalWrite(MEASURE,HIGH);
 
-  if(!ccs.begin(0x5B)){
+  if(!ccs.begin()){
     Serial.println("Failed to start sensor! Please check your wiring.");
     while(1);
   }
@@ -68,73 +72,48 @@ void setup() {
   while(!ccs.available());
   ds.temperature = ccs.calculateTemperature();
   ccs.setTempOffset(ds.temperature - 25.0);
-
-  SPI.begin(SCK, MISO, MOSI, CS);
-  LoRa.setPins(SS, RST, DI0);
-  
-  Serial.begin(9600);
 }
 
 void loop() { 
-  uint16_t dummy;
-  read_Temperature();
-  dummy = read_CO2();
-  cap_tvoc = read_CO2_TVOC();
-
-  Serial.print("Température : ");
-  Serial.print(ds.temperature);
-  Serial.print("\tCO2 : ");
-  Serial.println(dummy);
+  read_Temperature_CO2_TVOC();
+  read_mac(ds.mac);
+  //Serial.println(ds.mac);
   delay(1000);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-void read_Temperature(){ //permet de récuperer la température
+void read_Temperature_CO2_TVOC(){
   char buff [10];
   
   if(ccs.available()){
     ds.temperature = ccs.calculateTemperature();
-  }else{
-    Serial.println("Sensor read ERROR!");
-    ccs.readData();
-  }
-  
+    if(!ccs.readData()){
+      ds.CO2 = ccs.geteCO2();
+      ds.CO2_TVOC = ccs.getTVOC();
+
+      Serial.println("CO2: ");
+      Serial.println(ds.CO2);
+      Serial.println("ppm, TVOC: ");
+      Serial.println(ds.CO2_TVOC);
+      Serial.println("ppb Temp:");
+      Serial.println(ds.temperature);
+      }else{
+        Serial.println("ERROR!");
+        while(1);
+        }
+        }
   sprintf(buff,"%.1f",ds.temperature); //Permet d'avoir une valeur un chiffre après la virgule
   ds.temperature = strtof(buff, NULL); //Char to int
+  Serial.println("ppb Temp:");
+  Serial.println(ds.temperature);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-uint16_t read_CO2(){ //Permet de récupere la mesure de dioxyde de carbone estimée stockée
-  //uint16_t CO2_info;
-  if(!ccs.readData()){
-    //CO2_info = ccs.geteCO2();
-    ds.CO2 = ccs.geteCO2();
-  }else{
-    Serial.println("Sensor read ERROR!");
-    ccs.readData();
-  }
- //return CO2_info;
- return ds.CO2;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-uint16_t read_CO2_TVOC(){ //permet de récupere la mesure des composés organiques volatils totaux stockés
-  uint16_t CO2_info;
-  if(!ccs.readData()){
-    CO2_info = ccs.getTVOC();
-  }else{
-    Serial.println("Sensor read ERROR!");
-    ccs.readData();
-  }
- return CO2_info;
-}
-/*
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 uint8_t read_mac(uint8_t *mac){//Permet de récuperer l'adresse mac
   esp_efuse_read_mac(mac);
   //Serial.printf("mac : %02X:%02X:%02X:%02X:%02X:%02X \n",mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
-
+/*
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 double read_Piezo(){
  microseconds = micros();
@@ -155,10 +134,10 @@ double read_Piezo(){
   
   return x;
 }
-
+*/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-double read_Son(){
- microseconds = micros();
+void read_Son(){
+  microseconds = micros();
   for(int i=0; i<samples; i++)
   {
       vReal[i] = analogRead(CHANNEL_SON);
@@ -172,8 +151,8 @@ double read_Son(){
   FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  
   FFT.Compute(vReal, vImag, samples, FFT_FORWARD); 
   FFT.ComplexToMagnitude(vReal, vImag, samples); 
-  double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
-  
-  return x;
+  //double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
+  ds.son = FFT.MajorPeak(vReal, samples, samplingFrequency);
+  //return x;
 }
-*/
+//*/
