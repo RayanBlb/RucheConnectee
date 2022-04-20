10908d911a51b0b0a6bf301dcd2fd1f705222627
RayanBlb <rayan.benlacheheb.cours@gmail.com>
2022-04-20 18:39:08 +0200

    Rayan - Maj - Ajout code de test composants

--- Code_projet/Main/donnees.h
+++ Code_projet/Main/donnees.h
@@ -1,27 +1,28 @@
-#include "Adafruit_CCS811.h" //librerie du capteur CO2
+#include <LITTLEFS.h>
+#include <driver/i2s.h>
+#include "Adafruit_CCS811.h" //librerie du capteur CO2 et temperature
 #include "arduinoFFT.h"
+#include "WiFi.h"
 
-// Variable defini
-#define MEASURE 13
-
-// Variable Son
-#define CHANNEL_SON 4
-
-// Variable Piezo
-#define CHANNEL_PIEZO 2
-
-// Taille payload
-#define PAYLOAD_LEN 20
+#include "ia.h"
+#include "audio.h"
+#include "config.h"
 
 // Structure données
 struct data_trame
 {
+  char type;
   uint8_t mac[6];
   double son;
   double piezo;
   uint16_t CO2;
   uint16_t CO2_TVOC;
   float Temperature;
+  uint8_t hygro;
+  uint16_t v_bat;
+  uint8_t charg_bat;
+  uint8_t IA;
+  int erreur;
 } ds;
 
 // Variable son
@@ -44,62 +45,40 @@ unsigned long microseconds_piezo;
 double vReal_piezo[samples_piezo];
 double vImag_piezo[samples_piezo];
 
-Adafruit_CCS811 ccs; // permet d'utiliser le capteur CO2 et Temperature
+Adafruit_CCS811 ccs; // permet d'utiliser le capteur CO2 et temperature
 
 arduinoFFT FFTP = arduinoFFT();
 arduinoFFT FFT = arduinoFFT();
 
-void setup()
+void setup_donnees()
 {
-
-  Serial.begin(9600);
-  pinMode(MEASURE, OUTPUT);
-  digitalWrite(MEASURE, HIGH);
 
   if (!ccs.begin(0x5B))
   {
     Serial.println("Failed to start sensor! Please check your wiring.");
-    while (1);
+    while (1)
+      ;
   }
 
   // Calibrage capteur température
-  while (!ccs.available());
+  while (!ccs.available())
+    ;
   ds.Temperature = ccs.calculateTemperature();
   ccs.setTempOffset(ds.Temperature - 25.0);
 
   // Calibrage capteur son
   sampling_period_us_piezo = round(1000000 * (1.0 / samplingFrequency_piezo));
   sampling_period_us = round(1000000 * (1.0 / samplingFrequency));
+
+  // Setup IA
+  LITTLEFSInit();
 }
 
-void loop()
-{
-  read_CO2();
-  read_CO2_TVOC();
-  read_Temperature();
-  //read_Piezo();
-  read_Son();
-  read_mac(ds.mac);
-  
-  //Serial.println(ds.piezo);
-  Serial.println("//");
-  Serial.println(ds.son);
-  Serial.println("//");
-  Serial.println(ds.Temperature);
-  Serial.println("//");
-  Serial.println(ds.CO2);
-  Serial.println("//");
-  Serial.println(ds.CO2_TVOC);
-  Serial.println("//");
-  Serial.printf("mac : %02X:%02X:%02X:%02X:%02X:%02X \n", ds.mac[0], ds.mac[1], ds.mac[2], ds.mac[3], ds.mac[4], ds.mac[5]);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void read_Temperature()
-{
+{ // permet de récuperer la température
   char buff[10];
 
-  if (!ccs.readData())
+  if (ccs.available())
   {
     ds.Temperature = ccs.calculateTemperature();
   }
@@ -114,9 +93,10 @@ void read_Temperature()
   sprintf(buff, "%.1f", ds.Temperature); // Permet d'avoir une valeur un chiffre après la virgule
   ds.Temperature = strtof(buff, NULL);   // Char to int
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void read_CO2()
-{
+{ // Permet de récupere la mesure de dioxyde de carbone estimée stockée
+
   if (!ccs.readData())
   {
     ds.CO2 = ccs.geteCO2();
@@ -127,9 +107,10 @@ void read_CO2()
     ccs.readData();
   }
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void read_CO2_TVOC()
-{
+{ // permet de récupere la mesure des composés organiques volatils totaux stockés
+
   if (!ccs.readData())
   {
     ds.CO2_TVOC = ccs.getTVOC();
@@ -140,15 +121,16 @@ void read_CO2_TVOC()
     ccs.readData();
   }
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void read_mac(uint8_t *mac)
-{
+{ // Permet de récuperer l'adresse mac
   esp_efuse_read_mac(mac);
+  // Serial.printf("mac : %02X:%02X:%02X:%02X:%02X:%02X \n",mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void read_Piezo()
 {
+  /*SAMPLING*/
   microseconds = micros();
   for (int i = 0; i < samples_piezo; i++)
   {
@@ -167,9 +149,9 @@ void read_Piezo()
   ds.piezo = FFTP.MajorPeak(vReal_piezo, samples_piezo, samplingFrequency_piezo);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void read_Son()
 {
+  /*SAMPLING*/
   microseconds = micros();
   for (int i = 0; i < samples; i++)
   {
@@ -186,4 +168,68 @@ void read_Son()
   FFT.Compute(vReal, vImag, samples, FFT_FORWARD);
   FFT.ComplexToMagnitude(vReal, vImag, samples);
   ds.son = FFT.MajorPeak(vReal, samples, samplingFrequency);
+}
+
+int info_ia()
+{
+  int resultat = 0;
+
+  recording_file_init();
+  i2sInit();
+  xTaskCreate(i2s_adc, "i2s_adc", 1024 * 2, NULL, 1, NULL);
+  delay(RECORD_TIME * 1000 + 2500);
+  resultat = ai();
+
+  return resultat;
+}
+
+void build_trame(uint8_t *payload)
+{ // permet de fabriquer la trame
+
+  ds.type = 'I';
+  read_mac(ds.mac);
+  read_Son();
+  read_Temperature();
+  read_CO2();
+  read_CO2_TVOC();
+  read_Piezo();
+
+  ds.hygro = 30;
+  ds.v_bat = 13;
+  ds.charg_bat = 255;
+  ds.IA = info_ia();
+
+  ds.erreur = 0;
+
+  Serial.printf("Trame before cast : %c | %X:%X:%X:%X:%X:%X | %f | %f | %d | %d | %f | %d | %d | %d | %d | %d\n", ds.type, ds.mac[0], ds.mac[1], ds.mac[2], ds.mac[3], ds.mac[4], ds.mac[5], ds.son, ds.Temperature, ds.CO2, ds.CO2_TVOC, ds.piezo, ds.hygro, ds.v_bat, ds.charg_bat, ds.IA, ds.erreur);
+
+  payload[0] = uint8_t(ds.type);
+
+  for (int i = 1; i < 7; i++)
+  {
+    payload[i] = uint8_t(ds.mac[i - 1]);
+  }
+
+  payload[7] = uint8_t((uint16_t(ds.son) & 0xFF00) >> 8);
+  payload[8] = uint8_t((uint16_t(ds.son) & 0x00FF));
+
+  payload[9] = uint8_t((uint16_t(ds.Temperature * 10) & 0xFF00) >> 8); // Multiplie la valeur de la température par 10 pour ne pas avoir de nombre a virgule
+  payload[10] = uint8_t((uint16_t(ds.Temperature * 10) & 0x00FF));
+
+  payload[11] = uint8_t((uint16_t(ds.CO2) & 0xFF00) >> 8);
+  payload[12] = uint8_t((uint16_t(ds.CO2) & 0x00FF));
+
+  payload[13] = uint8_t((uint16_t(ds.CO2_TVOC) & 0xFF00) >> 8);
+  payload[14] = uint8_t((uint16_t(ds.CO2_TVOC) & 0x00FF));
+
+  payload[15] = uint8_t((uint16_t(ds.piezo) & 0xFF00) >> 8);
+  payload[16] = uint8_t((uint16_t(ds.piezo) & 0x00FF));
+
+  payload[17] = ds.hygro;
+
+  payload[18] = ds.v_bat;
+
+  payload[19] = ds.charg_bat;
+
+  payload[20] = ds.IA;
 }
--- Code_test/Code_test_sleep/Code_test_sleep.ino
+++ Code_test/Code_test_sleep/Code_test_sleep.ino
@@ -1,13 +1,11 @@
-void setup() {
- Serial.begin(9600);
- Serial.println("setup");
- //delay(1000);
-}
-
-void loop() {
-/*  Serial.print("Before : ");
- esp_sleep_enable_timer_wakeup(3000000); //30 seconds
- int ret = esp_light_sleep_start();
- Serial.print("light_sleep:");
- Serial.println(ret);*/
-}
+void setup() { 
+ Serial.begin(115200); 
+ Serial.println("setup"); 
+} 
+ 
+void loop() { 
+ esp_sleep_enable_timer_wakeup(3000000); //3 seconds 
+ int ret = esp_light_sleep_start(); 
+ Serial.print("light_sleep:"); 
+ Serial.println(ret); 
+} 
